357c4090ef95df3cb996ecac4f4eb13f44a2fdf6
MilaBozhevskaya <mi.bozhev@gmail.com>
2024-07-18 16:09:41 +0300

    feat: add styled of tooltip position

--- src/components/receivers/ReceiversTooltip.tsx
+++ src/components/receivers/ReceiversTooltip.tsx
@@ -2,7 +2,7 @@ import type { FC, PropsWithChildren } from 'react'
 import { styled } from 'styled-components'
 
 type ReceiversTooltipProps = PropsWithChildren<{
-  coords: { top: number; left: number }
+  coords: { top: number; left: number; height: number }
   list: string[]
 }>
 
@@ -12,7 +12,7 @@ export const ReceiversTooltip: FC<ReceiversTooltipProps> = ({
 }) => {
   return (
     <TooltipStyled top={`${coords.top}px`} left={`${coords.left}px`}>
-      <TooltipContainerStyled>
+      <TooltipContainerStyled height={`${coords.height}px`}>
         <TooltipListStyled>
           {list.map(el => (
             <TooltipItemStyled key={el}>{el}</TooltipItemStyled>
@@ -26,13 +26,27 @@ export const ReceiversTooltip: FC<ReceiversTooltipProps> = ({
 const TooltipStyled = styled.div<{ top: string; left: string }>`
   position: absolute;
   display: flex;
-  padding: 8px 12px;
+  padding: 6px 6px;
   background-color: rgba(255, 255, 255, 0.9);
   border-radius: 4px;
+  border: var(--border-style-tooltip);
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   top: ${props => props.top};
   left: ${props => props.left};
 `
-const TooltipContainerStyled = styled.div``
-const TooltipListStyled = styled.ul``
+const TooltipContainerStyled = styled.div<{ height: string }>`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  max-height: ${props => props.height};
+  border: var(--border-style-tooltip);
+  overflow-y: scroll;
+  padding-right: 12px;
+`
+const TooltipListStyled = styled.ol`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`
 const TooltipItemStyled = styled.li``
